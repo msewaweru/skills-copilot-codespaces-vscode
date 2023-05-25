b5e41311470f7b40d8c941dfe7fdc69db8789997
Eunice Waweru <73849846+msewaweru@users.noreply.github.com>
2023-05-25 11:15:25 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'React', 'Node'];
+    var member = {
+        name: 'Alex',
+        age: 20,
+        skills: skills
+    };
+    return member;
+}
