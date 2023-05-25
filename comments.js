b6c98f52422058e640421ee9ffc99831d4d07be0
Eunice Waweru <73849846+msewaweru@users.noreply.github.com>
2023-05-25 11:28:58 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,29 @@
+// Create web server
+// Create a web server that can respond to requests for /comments.json
+// with a JSON-encoded array of comment data.
+// We can use the same technique to serve JSON in response to any
+// kind of request.
+// We can use the same technique to serve JSON in response to any
+// kind of request.
+// We can use the same technique to serve JSON in response to any
+// kind of request.
+// We can use the same technique to serve JSON in response to any
+// kind of request.
+// We can use the same technique to serve JSON in response to any
+// kind of request.
+// We can use the same technique to serve JSON in response to any
+// kind of request.
+// We can use the same technique to serve JSON in response to any
+// kind of request.
+// We can use the same technique to serve JSON in response to any
+// kind of request.
+// We can use the same technique to serve JSON in response to any
+// kind of request.
+// We can use the same technique to serve JSON in response to any
+// kind of request.
+
+var http = require('http');
+var fs = require('fs');
+
+var comments = [
+
